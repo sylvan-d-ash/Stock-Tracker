ab1f7485ac3f14400f782fd66332d2ec0886e3d3
Sylvan Ash <sylvan.d.ash@gmail.com>
2025-04-17 00:25:17 +0300

    Update date formatters to be static

--- Stock Tracker/Extensions/TimeInterval.swift
+++ Stock Tracker/Extensions/TimeInterval.swift
@@ -7,29 +7,45 @@
 
 import Foundation
 
+private enum DateFormatters {
+    static let fullDate: DateFormatter = {
+        let formatter = DateFormatter()
+        formatter.dateFormat = "MMMM d, yyyy"
+        formatter.timeZone = TimeZone(abbreviation: "UTC")
+        return formatter
+    }()
+
+    static let dateTime: DateFormatter = {
+        let formatter = DateFormatter()
+        formatter.dateFormat = "MMM d h:mm:ss a"
+        formatter.timeZone = TimeZone(abbreviation: "UTC")
+        return formatter
+    }()
+
+    static let timeOnly: DateFormatter = {
+        let formatter = DateFormatter()
+        formatter.dateFormat = "h:mm:ss a"
+        formatter.timeZone = TimeZone(abbreviation: "UTC")
+        return formatter
+    }()
+}
+
 extension TimeInterval {
     func asDateString(with offsetMilliseconds: Int) -> String {
         let interval = self + TimeInterval(offsetMilliseconds / 1000)
         let date = Date(timeIntervalSince1970: interval)
-        let dateFormatter = DateFormatter()
-        dateFormatter.dateFormat = "MMMM d, yyyy"
-        return dateFormatter.string(from: date)
+        return DateFormatters.fullDate.string(from: date)
     }
 
     func asDateTimeString(with offsetMilliseconds: Int) -> String {
         let interval = self + TimeInterval(offsetMilliseconds / 1000)
         let date = Date(timeIntervalSince1970: interval)
-        let dateFormatter = DateFormatter()
-        dateFormatter.timeZone = TimeZone(abbreviation: "UTC")
-        dateFormatter.dateFormat = "MMM d h:mm:ss a"
-        return dateFormatter.string(from: date)
+        return DateFormatters.dateTime.string(from: date)
     }
 
     func asTimeString(with offsetMilliseconds: Int) -> String {
         let interval = self + TimeInterval(offsetMilliseconds / 1000)
         let date = Date(timeIntervalSince1970: interval)
-        let dateFormatter = DateFormatter()
-        dateFormatter.dateFormat = "h:mm:ss a"
-        return dateFormatter.string(from: date)
+        return DateFormatters.timeOnly.string(from: date)
     }
 }
