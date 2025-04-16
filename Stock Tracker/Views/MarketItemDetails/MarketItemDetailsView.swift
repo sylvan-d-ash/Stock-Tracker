cef6487eb1e1a8e2dcbfe1c9ca9282257caec161
Sylvan Ash <sylvan.d.ash@gmail.com>
2025-04-16 14:29:22 +0300

    Add MarketItemDetailsView

---
+++ Stock Tracker/Views/MarketItemDetails/MarketItemDetailsView.swift
@@ -0,0 +1,18 @@
+//
+//  MarketItemDetailsView.swift
+//  Stock Tracker
+//
+//  Created by Sylvan  on 16/04/2025.
+//
+
+import SwiftUI
+
+struct MarketItemDetailsView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    MarketItemDetailsView()
+}
