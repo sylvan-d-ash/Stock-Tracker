a181d05d4abd1c5a6790641e77b189fc073953a5
Sylvan Ash <sylvan.d.ash@gmail.com>
2025-04-16 12:46:22 +0300

    Preliminary market list implementation

--- Stock Tracker/StocksListView.swift
+++ Stock Tracker/StocksListView.swift
@@ -8,14 +8,43 @@
 import SwiftUI
 
 struct StocksListView: View {
+    @StateObject private var viewModel = StocksListViewModel()
+
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        NavigationView {
+            List(viewModel.markets, id: \.symbol) { item in
+                HStack {
+                    VStack(alignment: .leading) {
+                        Text(item.symbol)
+                            .font(.headline)
+                        Text(item.shortName)
+                            .font(.subheadline)
+                            .foregroundStyle(Color.gray)
+                    }
+
+                    Spacer()
+
+                    VStack(alignment: .trailing) {
+                        Text(item.regularMarketPrice?.fmt ?? "--")
+                            .font(.subheadline)
+                            .bold()
+
+                        HStack(spacing: 4) {
+                            Image(systemName: item.change > 0 ? "arrow.up.right" : item.change < 0 ? "arrow.down.right" : "minus")
+
+                            Text(String(format: "%.2f%%", item.change))
+                        }
+                        .font(.caption)
+                        .foregroundStyle((item.change > 0) ? Color.green : (item.change < 0 ? Color.red : Color.gray))
+                    }
+                }
+                .padding(.vertical, 6)
+            }
+            .navigationTitle("Market Summary")
+            .onAppear {
+                viewModel.fetchMarketData()
+            }
         }
-        .padding()
     }
 }
 
